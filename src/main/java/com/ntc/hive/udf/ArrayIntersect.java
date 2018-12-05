06af3991a22565d2c715bec1bca80ae1fb785a6b
ericxsun <eric.x.sun@gmail.com>
2018-12-05 18:45:38 +0800

    new funcs

---
+++ src/main/java/com/ntc/hive/udf/ArrayIntersect.java
@@ -0,0 +1,41 @@
+package com.ntc.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Copyright @2018 R&D, NTC Inc. (ntc.com)
+ *
+ * Author: Eric x.sun <eric.x.sun@gmail.com>
+ */
+
+
+@Description(name = "ArrayIntersect",
+    value = "_FUNC_(values) - Computes the intersection of the array arguments.  Note that ordering will be lost.")
+public class ArrayIntersect extends UDF {
+    public ArrayList<Text> evaluate(ArrayList<Text>... arrays) {
+        HashSet<Text> result_set = null;
+
+        for(ArrayList<Text> arr: arrays) {
+            if (arr == null) {
+                continue;
+            }
+
+            if (result_set == null) {
+                result_set = new HashSet<>(arr);
+            } else {
+                result_set.retainAll(arr);
+            }
+        }
+
+        if (result_set != null) {
+            return new ArrayList<>(result_set);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ src/main/java/com/ntc/hive/udf/LowerUpperCase.java
@@ -0,0 +1,30 @@
+package com.ntc.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.Description;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+import java.util.ArrayList;
+
+/**
+ * Copyright @2018 R&D, NTC Inc. (ntc.com)
+ *
+ * Author: Eric x.sun <eric.x.sun@gmail.com>
+ */
+
+
+@Description(name = "LowerCase", value = "_FUNC_(values, isLower) - lower or upper the elements.")
+public class LowerUpperCase extends UDF {
+    public ArrayList<String> evaluate(ArrayList<String> words, Boolean isLower) {
+        ArrayList<String> casedWords = new ArrayList<>();
+
+        for (String w: words) {
+            if (isLower) {
+                casedWords.add(w.toLowerCase());
+            } else {
+                casedWords.add(w.toUpperCase());
+            }
+        }
+
+        return casedWords;
+    }
+}
